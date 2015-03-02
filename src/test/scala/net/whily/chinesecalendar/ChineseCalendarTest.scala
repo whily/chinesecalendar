8d38cfa7bb64cda33a945af60e5abc09228439ed
Yujian Zhang <yujian.zhang@gmail.com>
2015-03-02 18:40:56 +0800

    Add test cases for ChineseCalendar.

--- src/test/scala/net/whily/chinesecalendar/ChineseCalendarTest.scala
+++ src/test/scala/net/whily/chinesecalendar/ChineseCalendarTest.scala
@@ -17,28 +17,34 @@ class ChineseCalendarTest extends FunSpec with Matchers {
     it("Check months()") {
       // It is interesting that I should use "shoulbe be ===" to test two arrays. 
       // Using "shoulbe be" always returns true, while using "shoulbe equal" cannot compile.
-      months("己未 己丑 戊午 戊子 丁巳 丁亥 丙辰 丙戌 丙辰 乙酉 乙卯 甲申") should be ===
-        (Array(Month("一月", "己未"), Month("二月", "己丑"), Month("三月", "戊午"),
-               Month("四月", "戊子"), Month("五月", "丁巳"), Month("六月", "丁亥"),
-               Month("七月", "丙辰"), Month("八月", "丙戌"), Month("九月", "丙辰"),
-               Month("十月", "乙酉"), Month("十一月", "乙卯"), Month("十二月", "甲申")))
-      months("甲寅 癸未 癸丑 壬午 壬子 辛巳 辛亥 庚辰 閏 庚戌 己卯 己酉 戊寅 戊申") should be ===
-        (Array(Month("一月", "甲寅"), Month("二月", "癸未"), Month("三月", "癸丑"),
-               Month("四月", "壬午"), Month("五月", "壬子"), Month("六月", "辛巳"),
-               Month("七月", "辛亥"), Month("八月", "庚辰"), Month("閏八月", "庚戌"), 
-               Month("九月", "己卯"), Month("十月", "己酉"), Month("十一月", "戊寅"), 
-               Month("十二月", "戊申")))
-      months("己卯 閏 戊申 戊寅 戊申 丁丑 丁未 丙子 丙午 乙亥 乙巳 甲戌 甲辰 癸酉") should be ===
-        (Array(Month("一月", "己卯"), Month("閏一月", "戊申"), Month("二月", "戊寅"), 
-               Month("三月", "戊申"), Month("四月", "丁丑"), Month("五月", "丁未"), 
-               Month("六月", "丙子"), Month("七月", "丙午"), Month("八月", "乙亥"), 
-               Month("九月", "乙巳"), Month("十月", "甲戌"), Month("十一月", "甲辰"), 
-               Month("十二月", "癸酉")))
-      months("己亥 己巳 進 戊戌 戊辰 丁酉 丙寅 丙申 乙丑 乙未 甲子 甲午") should be ===
-      (Array(Month("一月", "己亥"), Month("二月", "己巳"), Month("四月", "戊戌"),
-             Month("五月", "戊辰"), Month("六月", "丁酉"), Month("七月", "丙寅"),
-             Month("八月", "丙申"), Month("九月", "乙丑"), Month("十月", "乙未"),
-             Month("十一月", "甲子"), Month("十二月", "甲午")))      
+      months("己未 己丑 戊午 戊子 丁巳 丁亥 丙辰 丙戌 丙辰 乙酉 乙卯 甲申") should be === (Array(
+        Month("一月", "己未"), Month("二月", "己丑"), Month("三月", "戊午"),
+        Month("四月", "戊子"), Month("五月", "丁巳"), Month("六月", "丁亥"),
+        Month("七月", "丙辰"), Month("八月", "丙戌"), Month("九月", "丙辰"),
+        Month("十月", "乙酉"), Month("十一月", "乙卯"), Month("十二月", "甲申")))
+      months("甲寅 癸未 癸丑 壬午 壬子 辛巳 辛亥 庚辰 閏 庚戌 己卯 己酉 戊寅 戊申") should be === (Array(
+        Month("一月", "甲寅"), Month("二月", "癸未"), Month("三月", "癸丑"),
+        Month("四月", "壬午"), Month("五月", "壬子"), Month("六月", "辛巳"),
+        Month("七月", "辛亥"), Month("八月", "庚辰"), Month("閏八月", "庚戌"),
+        Month("九月", "己卯"), Month("十月", "己酉"), Month("十一月", "戊寅"),
+        Month("十二月", "戊申")))
+      months("己卯 閏 戊申 戊寅 戊申 丁丑 丁未 丙子 丙午 乙亥 乙巳 甲戌 甲辰 癸酉") should be === (Array(
+        Month("一月", "己卯"), Month("閏一月", "戊申"), Month("二月", "戊寅"),
+        Month("三月", "戊申"), Month("四月", "丁丑"), Month("五月", "丁未"),
+        Month("六月", "丙子"), Month("七月", "丙午"), Month("八月", "乙亥"),
+        Month("九月", "乙巳"), Month("十月", "甲戌"), Month("十一月", "甲辰"),
+        Month("十二月", "癸酉")))
+      months("己亥 己巳 進 戊戌 戊辰 丁酉 丙寅 丙申 乙丑 乙未 甲子 甲午") should be === (Array(
+        Month("一月", "己亥"), Month("二月", "己巳"), Month("四月", "戊戌"),
+        Month("五月", "戊辰"), Month("六月", "丁酉"), Month("七月", "丙寅"),
+        Month("八月", "丙申"), Month("九月", "乙丑"), Month("十月", "乙未"),
+        Month("十一月", "甲子"), Month("十二月", "甲午")))
+      months("丁亥 丁巳 丙戌 丙辰 乙酉 乙卯 甲申 甲寅 癸未 癸丑 壬午 壬子 後 壬午") should be === (Array(
+        Month("一月", "丁亥"), Month("二月", "丁巳"), Month("三月", "丙戌"),
+        Month("四月", "丙辰"), Month("五月", "乙酉"), Month("六月", "乙卯"),
+        Month("七月", "甲申"), Month("八月", "甲寅"), Month("九月", "癸未"),
+        Month("十月", "癸丑"), Month("十一月", "壬午"), Month("十二月", "壬子"),
+        Month("後十二月", "壬午")))
     }
 
     it("Check date.") {
@@ -62,6 +68,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       toDate("元始四年二月十一") should be (date(4, 3, 20))
 
       toDate("景初元年四月初一") should be (date(237, 4, 13))
+      toDate("景初三年後十二月初一") should be (date(240, 1, 12))      
     }
 
     it("Check data sanity.") {
