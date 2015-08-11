5d7d14f56470189587081ef58ca8549c0bf81f57
Yujian Zhang <yujian.zhang@gmail.com>
2015-08-11 20:46:14 +0800

    Add more data for Chinese Calendar table. Remove trailing whitespaces.

--- src/test/scala/net/whily/chinesecalendar/ChineseCalendarTest.scala
+++ src/test/scala/net/whily/chinesecalendar/ChineseCalendarTest.scala
@@ -1,9 +1,9 @@
 /**
  * Test cases for ChineseCalendar.scala.
- * 
+ *
  * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
  *
- * License: 
+ * License:
  *   GNU General Public License v2
  *   http://www.gnu.org/licenses/gpl-2.0.html
  * Copyright (C) 2014 Yujian Zhang
@@ -12,7 +12,7 @@
 import net.whily.chinesecalendar.ChineseCalendar._
 import net.whily.chinesecalendar.Chinese._
 import org.scalatest._
- 
+
 class ChineseCalendarTest extends FunSpec with Matchers {
   describe("Test Chinese Calendar calculation") {
     it("Check months()") {
@@ -64,36 +64,39 @@ class ChineseCalendarTest extends FunSpec with Matchers {
     }
 
     it("Check parseDate().") {
-      parseDate("漢武帝元朔六年") should be (parseDate("漢武帝元朔六年十月初一"))                  
+      parseDate("漢武帝元朔六年") should be (parseDate("漢武帝元朔六年十月初一"))
     }
 
     it("Check toDate().") {
-      // Check historical dates from zh.wikipedia.org.
+      // Check dates according to 三千五百年历日天象 (张培瑜 著)
+      toDate("秦昭襄王元年十一月") should be (date(-306, 12, 8))
       // TODO: 秦孝文王即位
       // TODO: toDate("秦孝文王元年十月己亥") should be (date(-250, 11, 12))
       toDate("秦孝文王元年十一月") should be (date(-250, 12, 19))
+
+      // Check historical dates from zh.wikipedia.org.
       // 秦王政即位
-      toDate("秦莊襄王三年五月丙午") should be (date(-246, 7, 6))              
+      toDate("秦莊襄王三年五月丙午") should be (date(-246, 7, 6))
       // 秦始皇崩 (http://blog.sina.com.cn/s/blog_50823c400100iqqw.html)
-      toDate("秦始皇三十七年八月丙寅") should be (date(-209, 9, 10))              
+      toDate("秦始皇三十七年八月丙寅") should be (date(-209, 9, 10))
       // 漢高祖崩
-      toDate("漢高祖十二年四月甲辰") should be (date(-194, 6, 1))        
+      toDate("漢高祖十二年四月甲辰") should be (date(-194, 6, 1))
       // 漢惠帝崩
-      toDate("漢惠帝七年八月戊寅") should be (date(-187, 9, 26))        
+      toDate("漢惠帝七年八月戊寅") should be (date(-187, 9, 26))
       // 漢高后崩
-      toDate("漢高后八年七月辛巳") should be (date(-179, 8, 18))        
+      toDate("漢高后八年七月辛巳") should be (date(-179, 8, 18))
       // 漢文帝崩
-      toDate("漢文帝後七年六月己亥") should be (date(-156, 7, 6))            
+      toDate("漢文帝後七年六月己亥") should be (date(-156, 7, 6))
       // 漢景帝崩
-      toDate("漢景帝後三年正月甲子") should be (date(-140, 3, 9))       
+      toDate("漢景帝後三年正月甲子") should be (date(-140, 3, 9))
       // 漢昭帝即位
-      toDate("漢武帝後元二年二月戊辰") should be (date(-86, 3, 30))      
+      toDate("漢武帝後元二年二月戊辰") should be (date(-86, 3, 30))
       // 漢昭帝崩
       toDate("漢昭帝元平元年四月癸未") should be (date(-73, 6, 5))
       // 昌邑王即位
       toDate("漢昭帝元平元年六月丙寅") should be (date(-73, 7, 18))
       // 昌邑王被废黜
-      toDate("漢昭帝元平元年六月癸巳") should be (date(-73, 8, 14))         
+      toDate("漢昭帝元平元年六月癸巳") should be (date(-73, 8, 14))
       // 漢宣帝即位
       toDate("漢昭帝元平元年七月庚申") should be (date(-73, 9, 10))
       // 漢宣帝崩
@@ -113,9 +116,9 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       toDate("漢平帝元始元年正月朔") should be (date(1, 2, 12))
       toDate("漢平帝元始元年正月初二") should be (date(1, 2, 13))
       toDate("漢平帝元始元年一月十一") should be (date(1, 2, 22))
-      toDate("漢平帝元始元年一月晦") should be (date(1, 3, 13))                  
+      toDate("漢平帝元始元年一月晦") should be (date(1, 3, 13))
       toDate("漢平帝元始元年二月朔") should be (date(1, 3, 14))
-      toDate("漢平帝元始元年二月十一") should be (date(1, 3, 24))      
+      toDate("漢平帝元始元年二月十一") should be (date(1, 3, 24))
       toDate("漢平帝元始元年二月己丑") should be (date(1, 3, 14))
       toDate("漢平帝元始元年二月己亥") should be (date(1, 3, 24))
       toDate("漢平帝元始元年二月己酉") should be (date(1, 4, 3))
@@ -147,7 +150,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
 
       // Check DynastyOrder.
       fromDate(date(543, 5, 6)) should === (List("梁武帝大同九年三月十七", "西魏文帝大統九年三月十七", "東魏孝靜帝武定元年三月十七"))
- 
+
       // Test for 唐武后
       fromDate(date(689, 12, 18)) should === (List("唐武后載初元年正月初一"))
     }
@@ -155,12 +158,12 @@ class ChineseCalendarTest extends FunSpec with Matchers {
     it("Check monthLength().") {
       monthLength("漢武帝太初元年十月") should be (29)
       monthLength("漢武帝太初元年九月") should be (29)
-      monthLength("漢武帝太初二年九月") should be (30)                  
+      monthLength("漢武帝太初二年九月") should be (30)
       monthLength("漢平帝元始元年正月") should be (30)
       monthLength("漢平帝元始元年二月") should be (29)
       monthLength("漢平帝元始元年十二月") should be (30)
       monthLength("魏高貴鄉公甘露元年十二月") should be (29)
-      monthLength("吳會稽王太平元年十二月") should be (30)            
+      monthLength("吳會稽王太平元年十二月") should be (30)
     }
 
     it("Check plusDays().") {
@@ -175,16 +178,16 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       parseDate("漢平帝元始元年").plusDays(30) should be (parseDate("漢平帝元始元年二月初一"))
       parseDate("漢平帝元始元年二月初一").plusDays(29) should be (parseDate("漢平帝元始元年三月初一"))
       parseDate("漢平帝元始元年二月初一").plusDays(60) should be (parseDate("漢平帝元始元年四月初二"))
-      parseDate("漢平帝元始元年十二月初一").plusDays(31) should be (parseDate("漢平帝元始二年一月初二"))            
+      parseDate("漢平帝元始元年十二月初一").plusDays(31) should be (parseDate("漢平帝元始二年一月初二"))
       parseDate("漢平帝元始元年二月己丑").plusDays(30) should be (parseDate("漢平帝元始元年三月初二"))
 
       // Diferent months, negative input.
       parseDate("漢平帝元始元年二月初一").plusDays(-30) should be (parseDate("漢平帝元始元年一月初一"))
       parseDate("漢平帝元始元年三月初一").plusDays(-29) should be (parseDate("漢平帝元始元年二月初一"))
       parseDate("漢平帝元始元年四月初二").plusDays(-60) should be (parseDate("漢平帝元始元年二月初一"))
-      parseDate("漢平帝元始二年一月初二").plusDays(-31) should be (parseDate("漢平帝元始元年十二月初一"))            
+      parseDate("漢平帝元始二年一月初二").plusDays(-31) should be (parseDate("漢平帝元始元年十二月初一"))
       parseDate("漢平帝元始元年三月初二").plusDays(-30) should be (parseDate("漢平帝元始元年二月初一"))
-      parseDate("漢平帝元始元年").plusDays(-1) should be (parseDate("漢哀帝元壽二年十二月廿九"))            
+      parseDate("漢平帝元始元年").plusDays(-1) should be (parseDate("漢哀帝元壽二年十二月廿九"))
     }
 
     it("Check containingSegments().") {
@@ -199,31 +202,31 @@ class ChineseCalendarTest extends FunSpec with Matchers {
         date(238, 2, 1), "蜀昭烈帝章武", "蜀後主延熙")
       val eraSegment4 = EraSegment("吳大帝嘉禾", parseDate("吳大帝嘉禾元年正月初一"),
         date(238, 8, 27), "吳大帝黃龍", "吳大帝赤烏")
-      containingSegments(date(234, 5, 6)) should be (List(eraSegment2, eraSegment3, eraSegment4))      
+      containingSegments(date(234, 5, 6)) should be (List(eraSegment2, eraSegment3, eraSegment4))
     }
 
     it("Check firstDayNextMonth().") {
       parseDate("漢惠帝七年九月初八").firstDayNextMonth(false) should be (parseDate("漢高后元年十月初一"))
-      parseDate("漢武帝元朔五年後九月十二").firstDayNextMonth(false) should be (parseDate("漢武帝元朔六年十月初一"))            
+      parseDate("漢武帝元朔五年後九月十二").firstDayNextMonth(false) should be (parseDate("漢武帝元朔六年十月初一"))
       parseDate("漢武帝元朔六年九月十四").firstDayNextMonth(false) should be (parseDate("漢武帝元狩元年十月初一"))
-      parseDate("漢武帝元封六年後九月十七").firstDayNextMonth(false) should be (parseDate("漢武帝太初元年十月初一"))                  
+      parseDate("漢武帝元封六年後九月十七").firstDayNextMonth(false) should be (parseDate("漢武帝太初元年十月初一"))
       parseDate("漢平帝元始元年二月己丑").firstDayNextMonth(false) should be (parseDate("漢平帝元始元年三月初一"))
-      parseDate("蜀昭烈帝章武三年四月初二").firstDayNextMonth(false) should be (parseDate("蜀後主建興元年五月初一"))      
+      parseDate("蜀昭烈帝章武三年四月初二").firstDayNextMonth(false) should be (parseDate("蜀後主建興元年五月初一"))
       parseDate("蜀後主炎興元年十一月十一").firstDayNextMonth(false) should be (parseDate("魏陳留王景元四年十二月初一"))
-      parseDate("隋文帝開皇二十年十二月朔").firstDayNextMonth(false) should be (parseDate("隋文帝仁壽元年一月初一"))                  
+      parseDate("隋文帝開皇二十年十二月朔").firstDayNextMonth(false) should be (parseDate("隋文帝仁壽元年一月初一"))
     }
 
     it("Check lastDayPrevMonth().") {
       parseDate("漢平帝元始元年三月初一").lastDayPrevMonth(false) should be (parseDate("漢平帝元始元年二月廿九"))
-      parseDate("蜀昭烈帝章武元年四月").lastDayPrevMonth(false) should be (parseDate("魏文帝黃初二年三月三十"))                  
-      parseDate("蜀後主建興元年五月初一").lastDayPrevMonth(false) should be (parseDate("蜀昭烈帝章武三年四月廿九"))      
+      parseDate("蜀昭烈帝章武元年四月").lastDayPrevMonth(false) should be (parseDate("魏文帝黃初二年三月三十"))
+      parseDate("蜀後主建興元年五月初一").lastDayPrevMonth(false) should be (parseDate("蜀昭烈帝章武三年四月廿九"))
     }
 
     it("Check sameDayNextMonth().") {
       parseDate("漢惠帝七年九月初八").sameDayNextMonth() should be (parseDate("漢高后元年十月初八"))
       parseDate("漢惠帝七年九月三十").sameDayNextMonth() should be (parseDate("漢高后元年十月晦"))
 
-      checkSameDayNextMonth()      
+      checkSameDayNextMonth()
     }
 
     it("Check sameDayPrevMonth().") {
@@ -243,7 +246,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       parseDate("魏明帝青龍五年一月初八").sameDayNextYear() should be (parseDate("魏明帝景初二年一月初八"))
       parseDate("魏明帝青龍五年二月初七").sameDayNextYear() should be (parseDate("魏明帝景初二年二月初七"))
 
-      checkSameDayNextYear()            
+      checkSameDayNextYear()
     }
 
     it("Check sameDayPrevYear().") {
@@ -257,11 +260,12 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       parseDate("魏明帝景初二年二月初七").sameDayPrevYear() should be (parseDate("魏明帝青龍五年二月初七"))
       parseDate("魏明帝景初二年三月初七").sameDayPrevYear() should be (parseDate("魏明帝青龍五年二月初七"))
 
-      checkSameDayPrevYear()                  
+      checkSameDayPrevYear()
     }
-    
+
     it("Check yearSexagenary().") {
       // BCEYears
+      parseDate("秦昭襄王五十四年").yearSexagenary() should be ("戊申")
       parseDate("漢武帝征和元年").yearSexagenary() should be ("己丑")
 
       // CEYears
@@ -272,13 +276,13 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       parseDate("蜀後主景耀三年").yearSexagenary() should be ("庚辰")
 
       // WuYears
-      parseDate("吳會稽王太平三年").yearSexagenary() should be ("戊寅")      
+      parseDate("吳會稽王太平三年").yearSexagenary() should be ("戊寅")
     }
 
     it("Check sexagenaries().") {
       sexagenaries("甲子", 3).mkString(" ") should be ("甲子 乙丑 丙寅")
       // Test wrap around.
-      sexagenaries("辛酉", 5).mkString(" ") should be ("辛酉 壬戌 癸亥 甲子 乙丑")      
+      sexagenaries("辛酉", 5).mkString(" ") should be ("辛酉 壬戌 癸亥 甲子 乙丑")
     }
 
     it("Check nextCharacter().") {
@@ -290,7 +294,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       nextCharacter("公元前5年2月") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9"))
       nextCharacter("公元前5年2月1") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "日"))
       nextCharacter("公元前5年2月12") should === (Array("日"))
-      nextCharacter("公元前5年2月2") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "日"))      
+      nextCharacter("公元前5年2月2") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "日"))
       nextCharacter("353年") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9"))
       nextCharacter("353年1") should === (Array("1", "2", "0", "月"))
       nextCharacter("353年2") should === (Array("月"))
@@ -299,7 +303,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       nextCharacter("353年2月1") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "日"))
       nextCharacter("353年2月12") should === (Array("日"))
       nextCharacter("353年2月2") should === (Array("1", "2", "3", "4", "5", "6", "7", "8", "0", "日"))
-      nextCharacter("353年2月3") should === (Array("日"))      
+      nextCharacter("353年2月3") should === (Array("日"))
       nextCharacter("353年3月3") should === (Array("1", "0", "日"))
       nextCharacter("353年4月3") should === (Array("0", "日"))
       nextCharacter("1582年10月") should === (Array("1", "2", "3", "4"))
@@ -309,7 +313,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       nextCharacter("秦王政元") should === (Array("年"))
       nextCharacter("秦王政十") should === (Array("一", "二", "三", "四", "五", "六", "七", "八", "九", "年"))
       nextCharacter("秦王政二") should === (Array("十", "年"))
-      nextCharacter("秦王政二十") should === (Array("一", "二", "三", "四", "五", "年"))                        
+      nextCharacter("秦王政二十") should === (Array("一", "二", "三", "四", "五", "年"))
       nextCharacter("秦始皇") should === (Array("二", "三"))
       nextCharacter("秦始皇二十") should === (Array("六", "七", "八", "九"))
       nextCharacter("秦始皇三十") should === (Array("一", "二", "三", "四", "五", "六", "七", "年"))
@@ -318,7 +322,7 @@ class ChineseCalendarTest extends FunSpec with Matchers {
       nextCharacter("秦始皇三十六年十二") should === (Array("月"))
       nextCharacter("秦始皇三十六年十二月") should === (Array("甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸", "初", "十", "二", "廿", "三", "朔", "晦"))
       nextCharacter("秦始皇三十六年十二月初") should === (Array("一", "二", "三", "四", "五", "六", "七", "八", "九", "十"))
-      nextCharacter("秦始皇三十六年十二月朔") should === (Array(""))                        
+      nextCharacter("秦始皇三十六年十二月朔") should === (Array(""))
       nextCharacter("漢獻帝中平六年") should === (Array("十", "閏"))
       nextCharacter("漢獻帝中平六年十") should === (Array("二"))
       nextCharacter("漢獻帝中平六年十二月") should === (Array("甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸", "初", "十", "二", "廿", "朔", "晦"))
